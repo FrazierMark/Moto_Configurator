3304c463fe931923414b4903ebc82ff8ca1d3da2
FrazierMark <m.mark.frazier@gmail.com>
2023-04-06 13:17:39 -0700

    add logos

--- src/Components/Overlay.jsx
+++ src/Components/Overlay.jsx
@@ -1,5 +1,17 @@
+import { Logo } from "./Logo";
+import { AiOutlineHighlight, AiOutlineShopping } from "react-icons/ai";
+
 export default function Overlay() {
-    return (
-        <h1>HELLO REACT</h1>
-    )
+  return <Intro />;
+}
+
+function Intro() {
+  return (
+    <div className="container">
+      <header>
+        <Logo width="40" height="40" />
+        <AiOutlineShopping size="3em" />
+      </header>
+    </div>
+  );
 }
